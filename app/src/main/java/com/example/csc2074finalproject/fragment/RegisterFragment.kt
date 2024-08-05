9178cc3f425e89fb7523867663d4660f184285bd
zmletters <zemingletters@gmail.com>
2024-08-06 02:10:36 +0800

    fixed alot of issues

--- app/src/main/java/com/example/csc2074finalproject/fragment/RegisterFragment.kt
+++ app/src/main/java/com/example/csc2074finalproject/fragment/RegisterFragment.kt
@@ -4,17 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
 import com.example.csc2074finalproject.R
-import com.example.csc2074finalproject.databinding.FragmentLoginBinding
+import com.example.csc2074finalproject.databinding.FragmentRegisterBinding
 import com.example.csc2074finalproject.viewModel.UserViewModel
 
-class LoginFragment: Fragment() {
-
-    private lateinit var binding: FragmentLoginBinding
+class RegisterFragment: Fragment() {
+    private lateinit var binding: FragmentRegisterBinding
     private val viewModel: UserViewModel by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,15 +24,16 @@ class LoginFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentLoginBinding.inflate(inflater, container, false)
-        binding.loginBtnLogin.setOnClickListener {
-            it.findNavController().navigate(R.id.action_loginFragment_to_userHomeFragment)
-        }
-        binding.loginRegisterHereTxt.setOnClickListener {
-            it.findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+
+        binding = FragmentRegisterBinding.inflate(inflater,container,false)
+        binding.registerBtnRegister.setOnClickListener {
+            val username: String = binding.registerUsername.text.toString()
+            val password: String = binding.registerPassword.text.toString()
+            val confpassword: String = binding.registerConfirmpassword.text.toString()
+
+            it.findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
         }
 
         return binding.root
     }
-
 }
